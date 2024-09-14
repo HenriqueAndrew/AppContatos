3c87bcdb91d7939105abb098518ca9ee16708dce
HenriqueAndrew <henriqueandrew1986_@hotmail.com>
2024-09-14 15:45:12 -0300

    Projeto para agenda de contatos utilizando framework Jetpack Compose

---
+++ app/src/main/java/br/edu/utfpr/appcontatos/data/Contact.kt
@@ -0,0 +1,16 @@
+package br.edu.utfpr.appcontatos.data
+
+data class Contact (
+    val id: Int = 0,
+    val firstName: String = "",
+    val lastName: String = "",
+    val phoneNumber: String = "",
+    val email: String = "",
+    val isFavorite: Boolean = false
+    )
+{
+    val fullName get() = "$firstName $lastName".trim()
+}
+
+
+
